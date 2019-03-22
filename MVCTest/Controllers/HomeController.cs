91d628dfed2642dc3855af530dc795ac0e09fc5c
Peterdevpro <peter.weng@devpro.com.tw>
2019-03-22 18:19:23 +0800

    model get post  DAY10

--- MVCTest/Controllers/HomeController.cs
+++ MVCTest/Controllers/HomeController.cs
@@ -1,31 +1,27 @@
-﻿using System;
+﻿using MVCTest.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using MVCTest.Models;
 
 namespace MVCTest.Controllers
 {
     public class HomeController : Controller
     {
-       public ActionResult Index()
+        public ActionResult Index()
         {
             DateTime date = DateTime.Now;
-            //Student data = new Student();
-            var list = new List<Student>
-            {
-                new Student("1", "小明", 80),
-                new Student("2", "小華", 70),
-                new Student("3", "小英", 60),
-                new Student("4", "小李", 50),
-                new Student("5", "小張", 90),
-            };  
-            
-                
             ViewBag.Date = date;
-            ViewBag.List = list;
-            return View();
+
+            Student data = new Student("1", "小明", 80);
+            return View(data);
+        }
+
+        public ActionResult Transcripts(string id, string name, int score)
+        {
+            Student data = new Student(id, name, score);
+            return View(data);
         }
     }
 }
