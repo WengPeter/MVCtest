81cad871c6924b87fbab87f5905980949e4cc7fd
Peterdevpro <peter.weng@devpro.com.tw>
2019-03-22 16:39:33 +0800

    day5

--- MVCTest/Controllers/HomeController.cs
+++ MVCTest/Controllers/HomeController.cs
@@ -12,15 +12,18 @@ namespace MVCTest.Controllers
        public ActionResult Index()
         {
             DateTime date = DateTime.Now;
-            Student data = new Student();
-            var list = new list<Student>
+            //Student data = new Student();
+            var list = new List<Student>
             {
-
+                new Student("1", "小明", 80),
+                new Student("2", "小華", 70),
+                new Student("3", "小英", 60),
+                new Student("4", "小李", 50),
+                new Student("5", "小張", 90),
             };  
             
                 
             ViewBag.Date = date;
-            ViewBag.Student = data;
             ViewBag.List = list;
             return View();
         }
--- MVCTest/Models/Student.cs
+++ MVCTest/Models/Student.cs
@@ -10,19 +10,17 @@ namespace MVCTest.Models
         public string id { get; set; }
         public string name { get; set; }
         public int score { get; set; }
-        public Student()
-        {
-            id = string.Empty;
-            name = string.Empty;
-            score = 0;
-        }
+        //public Student()
+        //{
+        //    id = string.Empty;
+        //    name = string.Empty;
+        //    score = 0;
+        //}
         public Student(string _id, string _name, int _score)
         {
-            new Student("1", "小明", 80);
-            new Student("2", "小華", 70);
-            new Student("3", "小英", 60);
-            new Student("4", "小李", 50);
-            new Student("5", "小張", 90);
+            id = _id;
+            name = _name;
+            score = _score;
         }
         public override string ToString()
         {
