1312fd5c0e24a00656190168f6862175d786c8a9
Peterdevpro <peter.weng@devpro.com.tw>
2019-03-22 16:07:57 +0800

    test

---
+++ MVCTest/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCTest.Models;
+
+namespace MVCTest.Controllers
+{
+    public class HomeController : Controller
+    {
+       public ActionResult Index()
+        {
+            DateTime date = DateTime.Now;
+            Student data = new Student();
+            var list = new list<Student>
+            {
+
+            };  
+            
+                
+            ViewBag.Date = date;
+            ViewBag.Student = data;
+            ViewBag.List = list;
+            return View();
+        }
+    }
+}
---
+++ MVCTest/Models/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCTest.Models
+{
+    public class Student
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public int score { get; set; }
+        public Student()
+        {
+            id = string.Empty;
+            name = string.Empty;
+            score = 0;
+        }
+        public Student(string _id, string _name, int _score)
+        {
+            new Student("1", "小明", 80);
+            new Student("2", "小華", 70);
+            new Student("3", "小英", 60);
+            new Student("4", "小李", 50);
+            new Student("5", "小張", 90);
+        }
+        public override string ToString()
+        {
+            return $"學號:{id}, 姓名:{name}, 分數:{score}.";
+        }
+    }
+}
